d04237fdc2575c55c9b12839374de2e5dc5574df
git <hi7sh@gmail.com>
2024-12-17 23:48:57 +0530

    adds Person entity

---
+++ src/main/java/com/splitprice/project/entity/Person.java
@@ -0,0 +1,19 @@
+package com.splitprice.project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String name;
+    private String email;
+}
