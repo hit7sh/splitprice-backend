c30e897c46910c49afe7e59e85a6154f317f1ecf
Hitesh Saini <hit7sh@gmail.com>
2024-12-26 02:51:50 +0530

    update-dependency

--- src/main/java/com/splitprice/project/Controller/PersonController.java
+++ src/main/java/com/splitprice/project/Controller/PersonController.java
@@ -8,6 +8,7 @@ import com.splitprice.project.dto.PersonDTO;
 import com.splitprice.project.entity.Expense;
 import com.splitprice.project.entity.Person;
 import com.splitprice.project.exceptionHandler.ArgumentMethodInValidException;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.transaction.Transactional;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
@@ -35,12 +36,14 @@ public class PersonController {
     private ModelMapper modelMapper;
 
     @PostMapping("/add")
+    @Operation(summary = "Greet user")
     public ResponseEntity<Person> addPerson(@RequestBody @Valid PersonDTO personDTO) throws MethodArgumentNotValidException, Exception {
         Person person = modelMapper.map(personDTO, Person.class);
         return ResponseEntity.status(HttpStatus.OK).body(personService.addPerson(person));
     }
 
     @GetMapping("/get-user/{email}")
+    @Operation(summary = "Greet email")
     public ResponseEntity<Person> getPerson(
             @PathVariable @Email(message = "Not a Valid Email Format") String email
     ) throws ConstraintViolationException, Exception {
@@ -48,6 +51,7 @@ public class PersonController {
     }
 
     @DeleteMapping("/delete-user/{email}")
+    @Operation(summary = "Greet Delete")
     public ResponseEntity<String> deletePerson(
             @PathVariable @Email(message = "Not a Valid Email Format") String email
     ) throws ConstraintViolationException, Exception {
@@ -55,6 +59,7 @@ public class PersonController {
     }
 
     @PostMapping("/add-friend/{user1}/{user2}")
+    @Operation(summary = "add friend")
     public ResponseEntity<String> addFriend(
             @PathVariable @Email(message = "Not a Valid Email Format") String user1,
             @PathVariable @Email(message = "Not a Valid Email Format") String user2
@@ -63,6 +68,7 @@ public class PersonController {
     }
 
     @PostMapping("/add-expense")
+    @Operation(summary = "add expense")
     public  ResponseEntity<String> addExpense(
             @RequestBody @Valid ExpenseRequestBodyDTO expenseRequestBodyDTO
     ) throws MethodArgumentNotValidException, Exception {
